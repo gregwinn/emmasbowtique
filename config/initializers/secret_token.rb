fc94ad8678fe358856db1cf50ccb2ec7fc76659a
Greg Winn <greg@winn.ws>
2015-06-29 22:33:21 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emmasbowtique::Application.config.secret_key_base = '8238accf3dee9020982181330e6f32cc13855951158cee32a28a6eac00dfab325e80b0fa66e61ccfaceec64de4f7f88501673edda36f408132db8d5c13a21870'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Emmasbowtique::Application.config.session_store :cookie_store, key: '_emmasbowtique_session'
